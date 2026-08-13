e3ff2ea066fed7b9b3ed208719249f9c73e46f63
Preetham S <spreetham6442@gmail.com>
2026-08-13 12:37:08 +0530

    chore: save studioHost in sanity.cli.js

--- studio/sanity.cli.js
+++ studio/sanity.cli.js
@@ -2,8 +2,8 @@ import { defineCliConfig } from 'sanity/cli'
 
 export default defineCliConfig({
   api: {
-    // MANUAL CONFIGURATION REQUIRED: Replace with your actual Sanity Project ID
     projectId: '12ok6v8i',
     dataset: 'production'
-  }
+  },
+  studioHost: 'drlohithdrlohithjj'
 })
